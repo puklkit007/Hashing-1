4790185dec5f27a52a2d0a662fedc91edf22f410
Pulkit Khandelwal <khandelwalpulkit007@gmail.com>
2025-10-08 00:30:50 -0500

    Create isomorphic_strings.py

---
+++ isomorphic_strings.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        
+        sHash, tHash = {}, {}
+
+        for x, y in zip(s, t):
+            if x in sHash:
+                if not sHash[x] == y:
+                    return False
+            else:
+                sHash[x] = y
+
+            if y in tHash:
+                if not tHash[y] == x:
+                    return False
+            else:
+                tHash[y] = x
+        
+        return True
